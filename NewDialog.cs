f01965479a2f610294215a4448c4886e3757fa44
5yyyyy <56665359+5yyyyy@users.noreply.github.com>
2022-03-14 15:01:20 +0800

    Add files via upload

---
+++ NewDialog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewDialog : MonoBehaviour
+{
+    private GameObject DialogUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogUI = transform.GetChild(0).GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            DialogUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            DialogUI.SetActive(false);
+        }
+    }
+}
