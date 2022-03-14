67a7df3428e4bd66bc51999f0ff93c64764187e6
5yyyyy <56665359+5yyyyy@users.noreply.github.com>
2022-03-14 15:04:14 +0800

    Update NewDialog.cs

--- NewDialog.cs
+++ NewDialog.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class NewDialog : MonoBehaviour
 {
-    private GameObject DialogUI;  //告示牌物体
+    private GameObject DialogUI;  //告示牌下UI物体
     
     void Start()
     {
-        DialogUI = transform.GetChild(0).GetChild(0).gameObject; //找到告示牌下的物体
+        DialogUI = transform.GetChild(0).GetChild(0).gameObject; //找到告示牌下的UI物体
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
