f562bbd3e4338e198c6c63b3b50bc1bcf4ec7cf1
5yyyyy <56665359+5yyyyy@users.noreply.github.com>
2022-03-14 15:03:24 +0800

    Update NewDialog.cs

--- NewDialog.cs
+++ NewDialog.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class NewDialog : MonoBehaviour
 {
-    private GameObject DialogUI;
-    // Start is called before the first frame update
+    private GameObject DialogUI;  //告示牌物体
+    
     void Start()
     {
-        DialogUI = transform.GetChild(0).GetChild(0).gameObject;
+        DialogUI = transform.GetChild(0).GetChild(0).gameObject; //找到告示牌下的物体
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
